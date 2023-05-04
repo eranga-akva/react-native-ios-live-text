f1af8afee6712625b3d2fff2571405bd6d28d70c
Eranga Dulshan <79952089+eranga-akva@users.noreply.github.com>
2023-05-04 19:38:49 +0530

    panning test
    panning test

--- ios/LiveTextImageViewViewManager.swift
+++ ios/LiveTextImageViewViewManager.swift
@@ -41,7 +41,7 @@ class LiveTextImageViewView : UIView {
             self._imageView = imageView
 
             // -- custom moon pinch to zoom start
-
+            
             self._imageView?.isUserInteractionEnabled = true
             self._imageView?.contentMode = .scaleAspectFit
             self._imageView?.addGestureRecognizer(pinchGesture)
@@ -50,6 +50,9 @@ class LiveTextImageViewView : UIView {
             self._imageView?.addGestureRecognizer(tapGesture)
             tapGesture.numberOfTapsRequired = 2
             tapGesture.addTarget(self, action: #selector(handleDoubleTap(_:)))
+            
+//            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+//            self._imageView?.addGestureRecognizer(panGesture)
 
             // -- custom moon pinch to zoom end
             
@@ -62,11 +65,68 @@ class LiveTextImageViewView : UIView {
                 }
             }
     }
+    
+//    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+//        guard let view = gesture.view else { return }
+//
+//        // Get the translation of the gesture
+//        let translation = gesture.translation(in: view.superview)
+//
+//        // Move the view by the translation
+//        view.center = CGPoint(x: view.center.x + translation.x,
+//                               y: view.center.y + translation.y)
+//
+//        // Reset the gesture's translation
+//        gesture.setTranslation(.zero, in: view.superview)
+//    }
+    
+    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+        guard let view = gesture.view else { return }
+        
+        // Check if the image is scaled beyond its original size
+        let isScaled = view.transform.a != 1.0 || view.transform.d != 1.0
+        
+        // Only allow panning if the image is scaled
+        if isScaled {
+            // Get the translation of the gesture
+            let translation = gesture.translation(in: view.superview)
+            
+            // Move the view by the translation
+            view.center = CGPoint(x: view.center.x + translation.x,
+                                   y: view.center.y + translation.y)
+            
+            // Reset the gesture's translation
+            gesture.setTranslation(.zero, in: view.superview)
+        }
+    }
 
+//    @objc func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+//        guard let view = gesture.view else { return }
+//
+//        view.transform = view.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+//        gesture.scale = 1.0
+//    }
+    
     @objc func handlePinch(_ gesture: UIPinchGestureRecognizer) {
         guard let view = gesture.view else { return }
-
-        view.transform = view.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        
+        // Calculate the midpoint of the pinch gesture
+        let pinchMidPoint = CGPoint(x: gesture.location(in: view).x - view.bounds.midX,
+                                    y: gesture.location(in: view).y - view.bounds.midY)
+        
+        // Translate the view's origin to the midpoint of the pinch gesture
+        var transform = view.transform.translatedBy(x: pinchMidPoint.x, y: pinchMidPoint.y)
+        
+        // Scale the view
+        transform = transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        
+        // Translate the view's origin back to its original position
+        transform = transform.translatedBy(x: -pinchMidPoint.x, y: -pinchMidPoint.y)
+        
+        // Set the view's transform
+        view.transform = transform
+        
+        // Reset the gesture's scale
         gesture.scale = 1.0
     }
 
